6b047baab29cf22a2b817dc5ecfbc68f853e0869
Andor Bibók <bibok.andor@gmail.com>
2013-11-29 16:16:06 +0100

    fontok

--- protected/modules/gallery/views/default/layout.php
+++ protected/modules/gallery/views/default/layout.php
@@ -7,20 +7,13 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
 
-	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans&subset=latin-ext" />
 	<link href="//fonts.googleapis.com/css?family=Open+Sans Condensed:300italic,300,700&subset=latin-ext" rel="stylesheet" type="text/css">
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/navigation.css" />
+	<link rel="stylesheet" type="text/css" href="css/fonts/os/sb/stylesheet.css" />
+	<link rel="stylesheet" type="text/css" href="css/fonts/os/reg/stylesheet.css" />
+	<link rel="stylesheet" type="text/css" href="css/fonts/os/li/stylesheet.css" />
 	
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
 
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-
+	
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -4,16 +4,10 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
-
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
-
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+	
+	<link rel="stylesheet" type="text/css" href="css/fonts/os/sb/stylesheet.css" />
+	<link rel="stylesheet" type="text/css" href="css/fonts/os/reg/stylesheet.css" />
+	<link rel="stylesheet" type="text/css" href="css/fonts/os/li/stylesheet.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
@@ -32,27 +26,7 @@
 	));
 ?>
 
-<div class="container" id="page">
-
-	
-	
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
-
-	<?php echo $content; ?>
-
-	<div class="clear"></div>
-
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
-
-</div><!-- page -->
+<?php echo $content; ?>
 
 </body>
 </html>
